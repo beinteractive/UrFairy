76388e936f494dbb6233b582bed4c4511ed48977
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-02-16 15:05:14 +0900

    最初のコミット

---
+++ Scripts/ColorExtensions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UrFairy {
+  public static class ColorExtensions {
+
+    public static Color Color(this int n) {
+      return (Color)n.Color32();
+    }
+
+    public static Color32 Color32(this int n) {
+      return new Color32((byte)((n >> 16) & 0xff), (byte)((n >> 8) & 0xff), (byte)((n >> 0) & 0xff), (byte)0xff);
+    }
+
+    public static Color R(this Color c, float r) {
+      c.r = r;
+      return c;
+    }
+
+    public static Color G(this Color c, float g) {
+      c.g = g;
+      return c;
+    }
+
+    public static Color B(this Color c, float b) {
+      c.b = b;
+      return c;
+    }
+
+    public static Color A(this Color c, float a) {
+      c.a = a;
+      return c;
+    }
+  }
+}
---
+++ Scripts/TransformExtensions.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UrFairy {
+  public static class TransformExtensions {
+
+    public class Transformation {
+      public Vector3 p;
+      public Quaternion r;
+      public Vector3 s;
+    }
+
+    public static void Identity(this Transform t) {
+      t.localPosition = Vector3.zero;
+      t.localRotation = Quaternion.identity;
+      t.localScale = Vector3.one;
+    }
+
+    public static void AutoRestore(this Transform t, System.Action f) {
+      var r = t.Replicate();
+      f();
+      t.Restore(r);
+    }
+
+    public static Transformation Replicate(this Transform t) {
+      var rep = new Transformation();
+      rep.p = t.localPosition;
+      rep.r = t.localRotation;
+      rep.s = t.localScale;
+      return rep;
+    }
+
+    public static void Restore(this Transform t, Transformation rep) {
+      t.localPosition = rep.p;
+      t.localRotation = rep.r;
+      t.localScale = rep.s;
+    }
+
+    public static Transform FindDescendant(this Transform t, string name) {
+      var l = t.childCount;
+      for (var i = 0; i < l; ++i) {
+        var child = t.GetChild(i);
+        if (child.name == name) {
+          return child;
+        }
+        var found = child.FindDescendant(name);
+        if (found != null) {
+          return found;
+        }
+      }
+      return null;
+    }
+  }
+}
