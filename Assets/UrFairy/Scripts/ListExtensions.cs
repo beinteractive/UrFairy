635bb419fa051a69846dec523b0fc14c454dc23b
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-02-16 15:05:14 +0900

    最初のコミット

---
+++ Assets/UrFairy/Scripts/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UrFairy {
+  public static class ListExtensions {
+    
+    public static void Shuffle<T>(this List<T> list) {
+      list.Sort((a, b) => {
+        return 1 - 2 * Random.Range(0, 1);
+      });
+    }
+  }
+}
