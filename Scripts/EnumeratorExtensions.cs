99e56c946c1860d92a78a18b4f886d98f7ebd937
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-02-22 17:34:43 +0900

    Enumerator Extensions

---
+++ Scripts/EnumeratorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UrFairy {
+  public static class EnumeratorExtensions {
+
+    public static IEnumerable<T> Enumerable<T>(this T o) {
+      yield return o;
+    }
+
+    public static IEnumerable<T> AddFirst<T>(this IEnumerable<T> enumerable, T element) {
+      yield return element;
+      foreach (var e in enumerable) {
+        yield return e;
+      }
+    }
+
+    public static IEnumerable<T> AddLast<T>(this IEnumerable<T> enumerable, T element) {
+      foreach (var e in enumerable) {
+        yield return e;
+      }
+      yield return element;
+    }
+  }
+}
