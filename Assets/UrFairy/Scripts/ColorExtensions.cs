711a21c0952fcf5df2680b46d8f2ca39ee4bcc62
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-02-24 10:58:44 +0900

    Color HSV

--- Assets/UrFairy/Scripts/ColorExtensions.cs
+++ Assets/UrFairy/Scripts/ColorExtensions.cs
@@ -31,5 +31,90 @@ namespace UrFairy {
       c.a = a;
       return c;
     }
+
+    public static Color R(this Color c, System.Func<float, float> f) {
+      c.r = f(c.r);
+      return c;
+    }
+
+    public static Color G(this Color c, System.Func<float, float> f) {
+      c.g = f(c.g);
+      return c;
+    }
+
+    public static Color B(this Color c, System.Func<float, float> f) {
+      c.b = f(c.b);
+      return c;
+    }
+
+    public static Color A(this Color c, System.Func<float, float> f) {
+      c.a = f(c.a);
+      return c;
+    }
+
+    public static HSV HSV(this Color c) {
+      return (HSV)c;
+    }
+
+    public static Color Color(this HSV hsv) {
+      return (Color)hsv;
+    }
+
+    public static HSV H(this HSV hsv, float h) {
+      hsv.h = h;
+      return hsv;
+    }
+
+    public static HSV S(this HSV hsv, float s) {
+      hsv.s = s;
+      return hsv;
+    }
+
+    public static HSV V(this HSV hsv, float v) {
+      hsv.v = v;
+      return hsv;
+    }
+
+    public static HSV A(this HSV hsv, float a) {
+      hsv.a = a;
+      return hsv;
+    }
+
+    public static HSV H(this HSV hsv, System.Func<float, float> f) {
+      hsv.h = f(hsv.h);
+      return hsv;
+    }
+
+    public static HSV S(this HSV hsv, System.Func<float, float> f) {
+      hsv.s = f(hsv.s);
+      return hsv;
+    }
+
+    public static HSV V(this HSV hsv, System.Func<float, float> f) {
+      hsv.v = f(hsv.v);
+      return hsv;
+    }
+
+    public static HSV A(this HSV hsv, System.Func<float, float> f) {
+      hsv.a = f(hsv.a);
+      return hsv;
+    }
+  }
+  public struct HSV {
+    public float h;
+    public float s;
+    public float v;
+    public float a;
+    public static explicit operator HSV(Color c) {
+      var hsv = new HSV();
+      Color.RGBToHSV(c, out hsv.h, out hsv.s, out hsv.v);
+      hsv.a = c.a;
+      return hsv;
+    }
+    public static explicit operator Color(HSV hsv) {
+      var c = Color.HSVToRGB(hsv.h, hsv.s, hsv.v);
+      c.a = hsv.a;
+      return c;
+    }
   }
 }
