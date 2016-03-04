cbc02830789edcf24c1a755eb29d929601774334
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-04 12:17:40 +0900

    Re-format

--- Assets/UrFairy/Editor/SpacebarHandTool.cs
+++ Assets/UrFairy/Editor/SpacebarHandTool.cs
@@ -4,37 +4,37 @@ using System.Collections;
 using System.Reflection;
 
 namespace UrFairy {
-  // Written by @kyubuns
-  // https://gist.github.com/kyubuns/04a4b80cfa7cb28d76bd
-  [InitializeOnLoad]
-  public class SpacebarHandTool {
-    static SpacebarHandTool() {
-      // OnGlobalEventHandler
-      // from http://anchan828.hatenablog.jp/entry/2013/12/29/015306
-      EditorApplication.CallbackFunction function = () => OnGlobalEventHandler(Event.current);
-      FieldInfo info = typeof(EditorApplication).GetField("globalEventHandler", BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
-      EditorApplication.CallbackFunction functions = (EditorApplication.CallbackFunction)info.GetValue(null);
-      functions += function;
-      info.SetValue(null, (object)functions);
-    }
+	// Written by @kyubuns
+	// https://gist.github.com/kyubuns/04a4b80cfa7cb28d76bd
+	[InitializeOnLoad]
+	public class SpacebarHandTool {
+		static SpacebarHandTool() {
+			// OnGlobalEventHandler
+			// from http://anchan828.hatenablog.jp/entry/2013/12/29/015306
+			EditorApplication.CallbackFunction function = () => OnGlobalEventHandler(Event.current);
+			FieldInfo info = typeof(EditorApplication).GetField("globalEventHandler", BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
+			EditorApplication.CallbackFunction functions = (EditorApplication.CallbackFunction)info.GetValue(null);
+			functions += function;
+			info.SetValue(null, (object)functions);
+		}
 
-    private static bool pressed;
-    private static Tool saved;
+		private static bool pressed;
+		private static Tool saved;
 
-    public static void OnGlobalEventHandler(Event e) {
-      if (!pressed && e.type == EventType.keyDown) {
-        if (e.keyCode == KeyCode.Space) {
-          pressed = true;
-          saved = Tools.current;
-          Tools.current = Tool.View;
-        }
-      }
-      if (pressed && e.type == EventType.keyUp) {
-        if (e.keyCode == KeyCode.Space) {
-          pressed = false;
-          Tools.current = saved;
-        }
-      }
-    }
-  }
+		public static void OnGlobalEventHandler(Event e) {
+			if (!pressed && e.type == EventType.keyDown) {
+				if (e.keyCode == KeyCode.Space) {
+					pressed = true;
+					saved = Tools.current;
+					Tools.current = Tool.View;
+				}
+			}
+			if (pressed && e.type == EventType.keyUp) {
+				if (e.keyCode == KeyCode.Space) {
+					pressed = false;
+					Tools.current = saved;
+				}
+			}
+		}
+	}
 }
--- Assets/UrFairy/Scripts/ListExtensions.cs
+++ Assets/UrFairy/Scripts/ListExtensions.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace UrFairy {
-  public static class ListExtensions {
+	public static class ListExtensions {
     
-    public static void Shuffle<T>(this List<T> list) {
-      list.Sort((a, b) => {
-        return 1 - 2 * Random.Range(0, 1);
-      });
-    }
-  }
+		public static void Shuffle<T>(this List<T> list) {
+			list.Sort((a, b) => {
+				return 1 - 2 * Random.Range(0, 1);
+			});
+		}
+	}
 }
