30e2d25ea6b1db6458d26076be84e3dfa3ecd033
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-23 11:15:51 +0900

    RendererExtensions / MaterialExtensions

---
+++ Scripts/MaterialExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+namespace UrFairy
+{
+	public static class MaterialExtensions
+	{
+		public static Material Color(this Material m, Func<Color, Color> f)
+		{
+			m.color = f(m.color);
+			return m;
+		}
+	}
+}
+
