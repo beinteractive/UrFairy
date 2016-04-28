0b42549f4d9311e427f3910c2ecb76c6d5b92184
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-04-28 11:04:51 +0900

    Re-formatting.

--- Assets/UrFairy/Scripts/ColorExtensions.cs
+++ Assets/UrFairy/Scripts/ColorExtensions.cs
@@ -1,123 +1,148 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
-namespace UrFairy {
-	public static class ColorExtensions {
+namespace UrFairy
+{
+    public static class ColorExtensions
+    {
+        public static Color Color(this int n)
+        {
+            return (Color)n.Color32();
+        }
 
-		public static Color Color(this int n) {
-			return (Color)n.Color32();
-		}
+        public static Color32 Color32(this int n)
+        {
+            return new Color32((byte)((n >> 16) & 0xff), (byte)((n >> 8) & 0xff), (byte)((n >> 0) & 0xff), (byte)0xff);
+        }
 
-		public static Color32 Color32(this int n) {
-			return new Color32((byte)((n >> 16) & 0xff), (byte)((n >> 8) & 0xff), (byte)((n >> 0) & 0xff), (byte)0xff);
-		}
+        public static Color R(this Color c, float r)
+        {
+            c.r = r;
+            return c;
+        }
 
-		public static Color R(this Color c, float r) {
-			c.r = r;
-			return c;
-		}
+        public static Color G(this Color c, float g)
+        {
+            c.g = g;
+            return c;
+        }
 
-		public static Color G(this Color c, float g) {
-			c.g = g;
-			return c;
-		}
+        public static Color B(this Color c, float b)
+        {
+            c.b = b;
+            return c;
+        }
 
-		public static Color B(this Color c, float b) {
-			c.b = b;
-			return c;
-		}
+        public static Color A(this Color c, float a)
+        {
+            c.a = a;
+            return c;
+        }
 
-		public static Color A(this Color c, float a) {
-			c.a = a;
-			return c;
-		}
+        public static Color R(this Color c, Func<float, float> f)
+        {
+            c.r = f(c.r);
+            return c;
+        }
 
-		public static Color R(this Color c, System.Func<float, float> f) {
-			c.r = f(c.r);
-			return c;
-		}
+        public static Color G(this Color c, Func<float, float> f)
+        {
+            c.g = f(c.g);
+            return c;
+        }
 
-		public static Color G(this Color c, System.Func<float, float> f) {
-			c.g = f(c.g);
-			return c;
-		}
+        public static Color B(this Color c, Func<float, float> f)
+        {
+            c.b = f(c.b);
+            return c;
+        }
 
-		public static Color B(this Color c, System.Func<float, float> f) {
-			c.b = f(c.b);
-			return c;
-		}
+        public static Color A(this Color c, Func<float, float> f)
+        {
+            c.a = f(c.a);
+            return c;
+        }
 
-		public static Color A(this Color c, System.Func<float, float> f) {
-			c.a = f(c.a);
-			return c;
-		}
+        public static HSV HSV(this Color c)
+        {
+            return (HSV)c;
+        }
 
-		public static HSV HSV(this Color c) {
-			return (HSV)c;
-		}
+        public static Color Color(this HSV hsv)
+        {
+            return (Color)hsv;
+        }
 
-		public static Color Color(this HSV hsv) {
-			return (Color)hsv;
-		}
+        public static HSV H(this HSV hsv, float h)
+        {
+            hsv.h = h;
+            return hsv;
+        }
 
-		public static HSV H(this HSV hsv, float h) {
-			hsv.h = h;
-			return hsv;
-		}
+        public static HSV S(this HSV hsv, float s)
+        {
+            hsv.s = s;
+            return hsv;
+        }
 
-		public static HSV S(this HSV hsv, float s) {
-			hsv.s = s;
-			return hsv;
-		}
+        public static HSV V(this HSV hsv, float v)
+        {
+            hsv.v = v;
+            return hsv;
+        }
 
-		public static HSV V(this HSV hsv, float v) {
-			hsv.v = v;
-			return hsv;
-		}
+        public static HSV A(this HSV hsv, float a)
+        {
+            hsv.a = a;
+            return hsv;
+        }
 
-		public static HSV A(this HSV hsv, float a) {
-			hsv.a = a;
-			return hsv;
-		}
+        public static HSV H(this HSV hsv, Func<float, float> f)
+        {
+            hsv.h = f(hsv.h);
+            return hsv;
+        }
 
-		public static HSV H(this HSV hsv, System.Func<float, float> f) {
-			hsv.h = f(hsv.h);
-			return hsv;
-		}
+        public static HSV S(this HSV hsv, Func<float, float> f)
+        {
+            hsv.s = f(hsv.s);
+            return hsv;
+        }
 
-		public static HSV S(this HSV hsv, System.Func<float, float> f) {
-			hsv.s = f(hsv.s);
-			return hsv;
-		}
+        public static HSV V(this HSV hsv, Func<float, float> f)
+        {
+            hsv.v = f(hsv.v);
+            return hsv;
+        }
 
-		public static HSV V(this HSV hsv, System.Func<float, float> f) {
-			hsv.v = f(hsv.v);
-			return hsv;
-		}
+        public static HSV A(this HSV hsv, Func<float, float> f)
+        {
+            hsv.a = f(hsv.a);
+            return hsv;
+        }
+    }
 
-		public static HSV A(this HSV hsv, System.Func<float, float> f) {
-			hsv.a = f(hsv.a);
-			return hsv;
-		}
-	}
+    public struct HSV
+    {
+        public float h;
+        public float s;
+        public float v;
+        public float a;
 
-	public struct HSV {
-		public float h;
-		public float s;
-		public float v;
-		public float a;
+        public static explicit operator HSV(Color c)
+        {
+            var hsv = new HSV();
+            Color.RGBToHSV(c, out hsv.h, out hsv.s, out hsv.v);
+            hsv.a = c.a;
+            return hsv;
+        }
 
-		public static explicit operator HSV(Color c) {
-			var hsv = new HSV();
-			Color.RGBToHSV(c, out hsv.h, out hsv.s, out hsv.v);
-			hsv.a = c.a;
-			return hsv;
-		}
-
-		public static explicit operator Color(HSV hsv) {
-			var c = Color.HSVToRGB(hsv.h, hsv.s, hsv.v);
-			c.a = hsv.a;
-			return c;
-		}
-	}
+        public static explicit operator Color(HSV hsv)
+        {
+            var c = Color.HSVToRGB(hsv.h, hsv.s, hsv.v);
+            c.a = hsv.a;
+            return c;
+        }
+    }
 }
--- Assets/UrFairy/Scripts/EnumeratorExtensions.cs
+++ Assets/UrFairy/Scripts/EnumeratorExtensions.cs
@@ -3,61 +3,72 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace UrFairy {
-	public static class EnumeratorExtensions {
+namespace UrFairy
+{
+    public static class EnumeratorExtensions
+    {
+        public static IEnumerable<T> Enumerable<T>(this T o)
+        {
+            yield return o;
+        }
 
-		public static IEnumerable<T> Enumerable<T>(this T o) {
-			yield return o;
-		}
+        public static IEnumerable<T> AddFirst<T>(this IEnumerable<T> enumerable, T element)
+        {
+            yield return element;
+            foreach (var e in enumerable)
+            {
+                yield return e;
+            }
+        }
 
-		public static IEnumerable<T> AddFirst<T>(this IEnumerable<T> enumerable, T element) {
-			yield return element;
-			foreach (var e in enumerable) {
-				yield return e;
-			}
-		}
+        public static IEnumerable<T> AddLast<T>(this IEnumerable<T> enumerable, T element)
+        {
+            foreach (var e in enumerable)
+            {
+                yield return e;
+            }
+            yield return element;
+        }
 
-		public static IEnumerable<T> AddLast<T>(this IEnumerable<T> enumerable, T element) {
-			foreach (var e in enumerable) {
-				yield return e;
-			}
-			yield return element;
-		}
+        public static void Each<T>(this IEnumerable<T> enumerable, System.Action<T> f)
+        {
+            foreach (var e in enumerable)
+            {
+                f(e);
+            }
+        }
 
-		public static void Each<T>(this IEnumerable<T> enumerable, System.Action<T> f) {
-			foreach (var e in enumerable) {
-				f(e);
-			}
-		}
+        public static void EachWithIndex<T>(this IEnumerable<T> enumerable, System.Action<T, int> f)
+        {
+            EachWithIndex(enumerable, 0, f);
+        }
 
-		public static void EachWithIndex<T>(this IEnumerable<T> enumerable, System.Action<T, int> f) {
-			EachWithIndex(enumerable, 0, f);
-		}
+        public static void EachWithIndex<T>(this IEnumerable<T> enumerable, int start, System.Action<T, int> f)
+        {
+            var list = new List<T>(enumerable);
+            var l = list.Count;
+            for (var i = 0; i < l; ++i)
+            {
+                f(list[i], start + i);
+            }
+        }
 
-		public static void EachWithIndex<T>(this IEnumerable<T> enumerable, int start, System.Action<T, int> f) {
-			var list = new List<T>(enumerable);
-			var l = list.Count;
-			for (var i = 0; i < l; ++i) {
-				f(list[i], start + i);
-			}
-		}
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> self)
+        {
+            return self.OrderBy(e => e, new RandomComparer<T>());
+        }
 
-		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> self)
-		{
-			return self.OrderBy(e => e, new RandomComparer<T>());
-		}
-
-		class RandomComparer<T> : IComparer<T>
-		{
-			public int Compare(T a, T b)
-			{
-				return 1 - 2 * Random.Range(0, 1);
-			}
-		}
+        class RandomComparer<T> : IComparer<T>
+        {
+            public int Compare(T a, T b)
+            {
+                return 1 - 2 * Random.Range(0, 1);
+            }
+        }
 
         public static T Sample<T>(this IEnumerable<T> self)
         {
             return self.ElementAt(Random.Range(0, self.Count()));
         }
-	}
+    }
 }
