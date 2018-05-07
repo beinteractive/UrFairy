f37e804316a40861db20be945188189ccca0c1d7
Yoshihiro Shindo <beinteractive.org@gmail.com>
2018-05-08 08:21:22 +0900

    Capture Screenshot

---
+++ Editor/CaptureScreenShot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UrFairy
+{
+    public class CaptureScreenShot
+    {
+        [MenuItem("UrFairy/Capture Screenshot")]
+        static void Capture()
+        {
+            ScreenCapture.CaptureScreenshot($"Screenshot-{DateTime.Now:yyyyMMdd-HHmmss}.png");
+        }
+    }
+}
